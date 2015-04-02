27cb4ea04490efc88c61c9c142c3350e0d32c246
Sang Kil Cha <sangkil.cha@gmail.com>
2015-04-01 20:58:52 -0400

    permission + a simple unittest

--- unittest/distance_test.sh
+++ unittest/distance_test.sh
@@ -17,6 +17,18 @@
 
 OUTPUTDIR=ofuzz-output
 
-echo "Hamming distance test started ..."
+echo "Hamming distance test started."
 rm -rf $OUTPUTDIR
-../ofuzz.native --triage --timeout=10 --gen-crash-tcs ../conf/test.conf
+echo "Fuzzing run."
+../ofuzz.native --triage --timeout=20 --gen-crash-tcs ./test.conf
+echo -n "Testcase check ... "
+for file in ofuzz-output/crashes/*
+do
+    diff=$(../utils/hamming $file testseed | awk -F, '{print $1}')
+    if [ "$diff" -ne "2" ]; then
+        echo "Test failure."
+        exit 1
+    fi
+done
+echo "passed."
+
